2d13d2d4ba42ede7c0c2ea80135316bcb8ae1ba7
Alicia Zhang <alicia.zhang@lotusflare.com>
2026-03-25 09:04:26 -0400

    add dag gnn

---
+++ dag_gnn/src/utils.py
@@ -0,0 +1,387 @@
+import numpy as np
+import torch
+from torch.utils.data.dataset import TensorDataset
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import scipy.linalg as slin
+import scipy.sparse as sp
+import networkx as nx
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import os
+import glob
+import re
+import pickle
+import math
+from torch.optim.adam import Adam
+
+# data generating functions
+
+def simulate_random_dag(d: int,
+                        degree: float,
+                        graph_type: str,
+                        w_range: tuple = (0.5, 2.0)) -> nx.DiGraph:
+    """Simulate random DAG with some expected degree.
+
+    Args:
+        d: number of nodes
+        degree: expected node degree, in + out
+        graph_type: {erdos-renyi, barabasi-albert, full}
+        w_range: weight range +/- (low, high)
+
+    Returns:
+        G: weighted DAG
+    """
+    if graph_type == 'erdos-renyi':
+        prob = float(degree) / (d - 1)
+        B = np.tril((np.random.rand(d, d) < prob).astype(float), k=-1)
+    elif graph_type == 'barabasi-albert':
+        m = int(round(degree / 2))
+        B = np.zeros([d, d])
+        bag = [0]
+        for ii in range(1, d):
+            dest = np.random.choice(bag, size=m)
+            for jj in dest:
+                B[ii, jj] = 1
+            bag.append(ii)
+            bag.extend(dest)
+    elif graph_type == 'full':  # ignore degree, only for experimental use
+        B = np.tril(np.ones([d, d]), k=-1)
+    else:
+        raise ValueError('unknown graph type')
+    # random permutation
+    P = np.random.permutation(np.eye(d, d))  # permutes first axis only
+    B_perm = P.T.dot(B).dot(P)
+    U = np.random.uniform(low=w_range[0], high=w_range[1], size=[d, d])
+    U[np.random.rand(d, d) < 0.5] *= -1
+    W = (B_perm != 0).astype(float) * U
+    G = nx.DiGraph(W)
+    return G
+
+
+def simulate_sem(G: nx.DiGraph,
+                 n: int, x_dims: int,
+                 sem_type: str,
+                 linear_type: str,
+                 noise_scale: float = 1.0) -> np.ndarray:
+    """Simulate samples from SEM with specified type of noise.
+
+    Args:
+        G: weigthed DAG
+        n: number of samples
+        sem_type: {linear-gauss,linear-exp,linear-gumbel}
+        noise_scale: scale parameter of noise distribution in linear SEM
+
+    Returns:
+        X: [n,d] sample matrix
+    """
+    W = nx.to_numpy_array(G)
+    d = W.shape[0]
+    X = np.zeros([n, d, x_dims])
+    ordered_vertices = list(nx.topological_sort(G))
+    assert len(ordered_vertices) == d
+    for j in ordered_vertices:
+        parents = list(G.predecessors(j))
+        if linear_type == 'linear':
+            eta = X[:, parents, 0].dot(W[parents, j])
+        elif linear_type == 'nonlinear_1':
+            eta = np.cos(X[:, parents, 0] + 1).dot(W[parents, j])
+        elif linear_type == 'nonlinear_2':
+            eta = (X[:, parents, 0]+0.5).dot(W[parents, j])
+        else:
+            raise ValueError('unknown linear data type')
+
+        if sem_type == 'linear-gauss':
+            if linear_type == 'linear':
+                X[:, j, 0] = eta + np.random.normal(scale=noise_scale, size=n)
+            elif linear_type == 'nonlinear_1':
+                X[:, j, 0] = eta + np.random.normal(scale=noise_scale, size=n)
+            elif linear_type == 'nonlinear_2':
+                X[:, j, 0] = 2.*np.sin(eta) + eta + np.random.normal(scale=noise_scale, size=n)
+        elif sem_type == 'linear-exp':
+            X[:, j, 0] = eta + np.random.exponential(scale=noise_scale, size=n)
+        elif sem_type == 'linear-gumbel':
+            X[:, j, 0] = eta + np.random.gumbel(scale=noise_scale, size=n)
+        else:
+            raise ValueError('unknown sem type')
+    if x_dims > 1 :
+        for i in range(x_dims-1):
+            X[:, :, i+1] = np.random.normal(scale=noise_scale, size=1)*X[:, :, 0] + np.random.normal(scale=noise_scale, size=1) + np.random.normal(scale=noise_scale, size=(n, d))
+        X[:, :, 0] = np.random.normal(scale=noise_scale, size=1) * X[:, :, 0] + np.random.normal(scale=noise_scale, size=1) + np.random.normal(scale=noise_scale, size=(n, d))
+    return X
+
+
+def count_accuracy(G_true: nx.DiGraph,
+                   G: nx.DiGraph,
+                   G_und: nx.DiGraph = None) -> tuple:
+    """Compute FDR, TPR, and FPR for B, or optionally for CPDAG B + B_und.
+
+    Args:
+        G_true: ground truth graph
+        G: predicted graph
+        G_und: predicted undirected edges in CPDAG, asymmetric
+
+    Returns:
+        fdr: (reverse + false positive) / prediction positive
+        tpr: (true positive) / condition positive
+        fpr: (reverse + false positive) / condition negative
+        shd: undirected extra + undirected missing + reverse
+        nnz: prediction positive
+    """
+    B_true = nx.to_numpy_array(G_true) != 0
+    B = nx.to_numpy_array(G) != 0
+    B_und = None if G_und is None else nx.to_numpy_array(G_und)
+    d = B.shape[0]
+    # linear index of nonzeros
+    if B_und is not None:
+        pred_und = np.flatnonzero(B_und)
+    pred = np.flatnonzero(B)
+    cond = np.flatnonzero(B_true)
+    cond_reversed = np.flatnonzero(B_true.T)
+    cond_skeleton = np.concatenate([cond, cond_reversed])
+    # true pos
+    true_pos = np.intersect1d(pred, cond, assume_unique=True)
+    if B_und is not None:
+        # treat undirected edge favorably
+        true_pos_und = np.intersect1d(pred_und, cond_skeleton, assume_unique=True)
+        true_pos = np.concatenate([true_pos, true_pos_und])
+    # false pos
+    false_pos = np.setdiff1d(pred, cond_skeleton, assume_unique=True)
+    if B_und is not None:
+        false_pos_und = np.setdiff1d(pred_und, cond_skeleton, assume_unique=True)
+        false_pos = np.concatenate([false_pos, false_pos_und])
+    # reverse
+    extra = np.setdiff1d(pred, cond, assume_unique=True)
+    reverse = np.intersect1d(extra, cond_reversed, assume_unique=True)
+    # compute ratio
+    pred_size = len(pred)
+    if B_und is not None:
+        pred_size += len(pred_und)
+    cond_neg_size = 0.5 * d * (d - 1) - len(cond)
+    fdr = float(len(reverse) + len(false_pos)) / max(pred_size, 1)
+    tpr = float(len(true_pos)) / max(len(cond), 1)
+    fpr = float(len(reverse) + len(false_pos)) / max(cond_neg_size, 1)
+    # structural hamming distance
+    B_lower = np.tril(B + B.T)
+    if B_und is not None:
+        B_lower += np.tril(B_und + B_und.T)
+    pred_lower = np.flatnonzero(B_lower)
+    cond_lower = np.flatnonzero(np.tril(B_true + B_true.T))
+    extra_lower = np.setdiff1d(pred_lower, cond_lower, assume_unique=True)
+    missing_lower = np.setdiff1d(cond_lower, pred_lower, assume_unique=True)
+    shd = len(extra_lower) + len(missing_lower) + len(reverse)
+    return fdr, tpr, fpr, shd, pred_size
+
+
+
+#========================================
+# VAE utility functions
+#========================================
+def load_data(args, batch_size=1000, suffix='', debug = False):
+    #  # configurations
+    n, d = args.data_sample_size, args.data_variable_size
+    graph_type, degree, sem_type, linear_type = args.graph_type, args.graph_degree, args.graph_sem_type, args.graph_linear_type
+    x_dims = args.x_dims
+
+    if args.data_type == 'synthetic':
+        # generate data
+        G = simulate_random_dag(d, degree, graph_type)
+        X = simulate_sem(G, n, x_dims, sem_type, linear_type)
+    elif args.data_type == 'generated':
+        # use generated data
+        data_path = os.path.join(args.data_dir, args.data_filename)
+        graph_path = os.path.join(args.data_dir, args.graph_file)
+        G = nx.DiGraph(pd.read_csv(graph_path, header=None).values)
+        X = pd.read_csv(data_path).values
+        X = X[:, :, np.newaxis]
+
+    feat_train = torch.FloatTensor(X)
+
+    # reconstruct itself
+    train_data = TensorDataset(feat_train, feat_train)
+
+    train_data_loader = DataLoader(train_data, batch_size=batch_size)
+
+    return train_data_loader, G
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
+def get_triu_indices(num_nodes):
+    """Linear triu (upper triangular) indices."""
+    ones = torch.ones(num_nodes, num_nodes)
+    eye = torch.eye(num_nodes, num_nodes)
+    triu_indices = (ones.triu() - eye).nonzero().t()
+    triu_indices = triu_indices[0] * num_nodes + triu_indices[1]
+    return triu_indices
+
+
+def get_tril_indices(num_nodes):
+    """Linear tril (lower triangular) indices."""
+    ones = torch.ones(num_nodes, num_nodes)
+    eye = torch.eye(num_nodes, num_nodes)
+    tril_indices = (ones.tril() - eye).nonzero().t()
+    tril_indices = tril_indices[0] * num_nodes + tril_indices[1]
+    return tril_indices
+
+
+def get_offdiag_indices(num_nodes):
+    """Linear off-diagonal indices."""
+    ones = torch.ones(num_nodes, num_nodes)
+    eye = torch.eye(num_nodes, num_nodes)
+    offdiag_indices = (ones - eye).nonzero().t()
+    offdiag_indices = offdiag_indices[0] * num_nodes + offdiag_indices[1]
+    return offdiag_indices
+
+
+def get_triu_offdiag_indices(num_nodes):
+    """Linear triu (upper) indices w.r.t. vector of off-diagonal elements."""
+    triu_idx = torch.zeros(num_nodes * num_nodes)
+    triu_idx[get_triu_indices(num_nodes)] = 1.
+    triu_idx = triu_idx[get_offdiag_indices(num_nodes)]
+    return triu_idx.nonzero()
+
+
+def get_tril_offdiag_indices(num_nodes):
+    """Linear tril (lower) indices w.r.t. vector of off-diagonal elements."""
+    tril_idx = torch.zeros(num_nodes * num_nodes)
+    tril_idx[get_tril_indices(num_nodes)] = 1.
+    tril_idx = tril_idx[get_offdiag_indices(num_nodes)]
+    return tril_idx.nonzero()
+
+
+def get_minimum_distance(data):
+    data = data[:, :, :, :2].transpose(1, 2)
+    data_norm = (data ** 2).sum(-1, keepdim=True)
+    dist = data_norm + \
+           data_norm.transpose(2, 3) - \
+           2 * torch.matmul(data, data.transpose(2, 3))
+    min_dist, _ = dist.min(1)
+    return min_dist.view(min_dist.size(0), -1)
+
+
+def kl_categorical(preds, log_prior, num_atoms, eps=1e-16):
+    kl_div = preds * (torch.log(preds + eps) - torch.log(log_prior + eps))
+    return kl_div.sum() / (num_atoms)
+
+def kl_gaussian(preds, zsize):
+    predsnew = preds.squeeze(1)
+    mu = predsnew[:,0:zsize]
+    log_sigma = predsnew[:,zsize:2*zsize]
+    kl_div = torch.exp(2*log_sigma) - 2*log_sigma + mu * mu
+    kl_sum = kl_div.sum()
+    return (kl_sum / (preds.size(0)) - zsize)*0.5
+
+def kl_gaussian_sem(preds):
+    mu = preds
+    kl_div = mu * mu
+    kl_sum = kl_div.sum()
+    return (kl_sum / (preds.size(0)))*0.5
+
+def kl_categorical_uniform(preds, num_atoms, num_edge_types, add_const=False,
+                           eps=1e-16):
+    kl_div = preds * torch.log(preds + eps)
+    if add_const:
+        const = np.log(num_edge_types)
+        kl_div += const
+    return kl_div.sum() / (num_atoms * preds.size(0))
+
+def nll_catogrical(preds, target, add_const = False):
+    '''compute the loglikelihood of discrete variables
+    '''
+    # loss = nn.CrossEntropyLoss()
+
+    total_loss = 0
+    for node_size in range(preds.size(1)):
+        total_loss += - (torch.log(preds[:,node_size, target[:, node_size].long()]) * target[:, node_size]).mean()
+
+    return total_loss
+
+def nll_gaussian(preds, target, variance, add_const=False):
+    mean1 = preds
+    mean2 = target
+    neg_log_p = variance + torch.div(torch.pow(mean1 - mean2, 2), 2.*np.exp(2. * variance))
+    if add_const:
+        const = 0.5 * torch.log(2 * torch.from_numpy(np.pi) * variance)
+        neg_log_p += const
+    return neg_log_p.sum() / (target.size(0))
+
+#Symmetrically normalize adjacency matrix.
+def normalize_adj(adj):
+    rowsum = torch.abs(torch.sum(adj,1))
+    d_inv_sqrt = torch.pow(rowsum, -0.5)
+    d_inv_sqrt[torch.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = torch.diag(d_inv_sqrt)
+    myr = torch.matmul(torch.matmul(d_mat_inv_sqrt,adj),d_mat_inv_sqrt)
+    myr[isnan(myr)] = 0.
+    return myr
+
+
+def preprocess_adj_new(adj):
+    adj_normalized = (torch.eye(adj.shape[0]).double() - (adj.transpose(0,1)))
+    return adj_normalized
+
+def preprocess_adj_new1(adj):
+    adj_normalized = torch.inverse(torch.eye(adj.shape[0]).double()-adj.transpose(0,1))
+    return adj_normalized
+
+def isnan(x):
+    return x!=x
+
+def my_normalize(z):
+    znor = torch.zeros(z.size()).double()
+    for i in range(z.size(0)):
+        testnorm = torch.norm(z[i,:,:], dim=0)
+        znor[i,:,:] = z[i,:,:]/testnorm
+    znor[isnan(znor)] = 0.0
+    return znor
+
+def sparse_to_tuple(sparse_mx):
+#    """Convert sparse matrix to tuple representation."""
+    def to_tuple(mx):
+        if not sp.isspmatrix_coo(mx):
+            mx = mx.tocoo()
+        coords = np.vstack((mx.row, mx.col)).transpose()
+        values = mx.data
+        shape = mx.shape
+        return coords, values, shape
+
+    if isinstance(sparse_mx, list):
+        for i in range(len(sparse_mx)):
+            sparse_mx[i] = to_tuple(sparse_mx[i])
+    else:
+        sparse_mx = to_tuple(sparse_mx)
+
+    return sparse_mx
+
+
+def matrix_poly(matrix, d):
+    x = torch.eye(d).double()+ torch.div(matrix, d)
+    return torch.matrix_power(x, d)
+
+
+# matrix loss: makes sure at least A connected to another parents for child
+def A_connect_loss(A, tol, z):
+    d = A.size()[0]
+    loss = 0
+    for i in range(d):
+        loss +=  2 * tol - torch.sum(torch.abs(A[:,i])) - torch.sum(torch.abs(A[i,:])) + z * z
+    return loss
+
+# element loss: make sure each A_ij > 0
+def A_positive_loss(A, z_positive):
+    result = - A + z_positive * z_positive
+    loss =  torch.sum(result)
+
+    return loss
